3bd0003622b08c9ae375a1e0a6515d0e114dee70
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-02 15:22:12 +0700

    Menambahkan fungsi MergeSort

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -26,3 +26,19 @@ void input()
     cout << "\n-------------------------------" << endl;
     cout << "\nInputkan Isi element array" << endl;
     cout << "-------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
